fe88f320d00dc2b7cf15ded251907d2cfcf81cb1
qwerty287 <ndev@web.de>
2022-01-09 19:22:01 +0100

    Sync JSON
    Closes #2

--- prism4j/src/main/java/io/noties/prism4j/languages/Prism_json.java
+++ prism4j/src/main/java/io/noties/prism4j/languages/Prism_json.java
@@ -9,21 +9,22 @@ import static java.util.regex.Pattern.CASE_INSENSITIVE;
 import static java.util.regex.Pattern.compile;
 
 @SuppressWarnings("unused")
-@Aliases("jsonp")
+@Aliases({"jsonp", "webmanifest"})
 public class Prism_json {
 
     @NotNull
     public static Prism4j.Grammar create(@NotNull Prism4j prism4j) {
         return grammar(
                 "json",
-                token("property", pattern(compile("\"(?:\\\\.|[^\\\\\"\\r\\n])*\"(?=\\s*:)", CASE_INSENSITIVE))),
+                token("property", pattern(compile("\"(?:\\\\.|[^\\\\\"\\r\\n])*\"(?=\\s*:)"), false, true)),
                 token("string", pattern(compile("\"(?:\\\\.|[^\\\\\"\\r\\n])*\"(?!\\s*:)"), false, true)),
-                token("number", pattern(compile("\\b0x[\\dA-Fa-f]+\\b|(?:\\b\\d+\\.?\\d*|\\B\\.\\d+)(?:[Ee][+-]?\\d+)?"))),
-                token("punctuation", pattern(compile("[{}\\[\\]);,]"))),
+                token("comment", pattern(compile("//.*|/\\*[\\s\\S]*?(?:\\*/|$)"), false, true)),
+                token("number", pattern(compile("-?\\b\\d+(?:\\.\\d+)?(?:e[+-]?\\d+)?\\b", CASE_INSENSITIVE))),
+                token("punctuation", pattern(compile("[{}\\[\\],]"))),
                 // not sure about this one...
                 token("operator", pattern(compile(":"))),
-                token("boolean", pattern(compile("\\b(?:true|false)\\b", CASE_INSENSITIVE))),
-                token("null", pattern(compile("\\bnull\\b", CASE_INSENSITIVE)))
+                token("boolean", pattern(compile("\\b(?:true|false)\\b"))),
+                token("null", pattern(compile("\\bnull\\b"), false, false, "keyword"))
         );
     }
 }
