0e0fe09880a99601a6bcfaba21c5f783037ad9fa
qwerty287 <ndev@web.de>
2022-01-10 21:27:37 +0100

    Sync C and C++

--- prism4j/src/main/java/io/noties/prism4j/languages/Prism_c.java
+++ prism4j/src/main/java/io/noties/prism4j/languages/Prism_c.java
@@ -5,6 +5,8 @@ import io.noties.prism4j.Prism4j;
 import io.noties.prism4j.annotations.Extend;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
+
 import static io.noties.prism4j.Prism4j.*;
 import static java.util.regex.Pattern.*;
 
@@ -15,32 +17,49 @@ public class Prism_c {
     @NotNull
     public static Prism4j.Grammar create(@NotNull Prism4j prism4j) {
 
+        final Token commentToken = token("comment", pattern(compile("//(?:[^\\r\\n\\\\]|\\\\(?:\\r\\n?|\\n|(?![\\r\\n])))*|/\\*[\\s\\S]*?(?:\\*/|$)"), false, true));
         final Grammar c = GrammarUtils.extend(
                 GrammarUtils.require(prism4j, "clike"),
                 "c",
                 token -> {
                     final String name = token.name();
-                    return !"class-name".equals(name) && !"boolean".equals(name);
+                    return !"boolean".equals(name);
                 },
-                token("keyword", pattern(compile("\\b(?:_Alignas|_Alignof|_Atomic|_Bool|_Complex|_Generic|_Imaginary|_Noreturn|_Static_assert|_Thread_local|asm|typeof|inline|auto|break|case|char|const|continue|default|do|double|else|enum|extern|float|for|goto|if|int|long|register|return|short|signed|sizeof|static|struct|switch|typedef|union|unsigned|void|volatile|while)\\b"))),
-                token("operator", pattern(compile("-[>-]?|\\+\\+?|!=?|<<?=?|>>?=?|==?|&&?|\\|\\|?|[~^%?*\\/]"))),
-                token("number", pattern(compile("(?:\\b0x[\\da-f]+|(?:\\b\\d+\\.?\\d*|\\B\\.\\d+)(?:e[+-]?\\d+)?)[ful]*", CASE_INSENSITIVE)))
+                commentToken,
+                token("string", pattern(compile("\"(?:\\\\(?:\\r\\n|[\\s\\S])|[^\"\\\\\\r\\n])*\""), false, true)),
+                token("class-name", pattern(compile("(\\b(?:enum|struct)\\s+(?:__attribute__\\s*\\(\\([\\s\\S]*?\\)\\)\\s*)?)\\w+|\\b[a-z]\\w*_t\\b"), true)),
+                token("keyword", pattern(compile("\\b(?:__attribute__|_Alignas|_Alignof|_Atomic|_Bool|_Complex|_Generic|_Imaginary|_Noreturn|_Static_assert|_Thread_local|asm|typeof|inline|auto|break|case|char|const|continue|default|do|double|else|enum|extern|float|for|goto|if|int|long|register|return|short|signed|sizeof|static|struct|switch|typedef|union|unsigned|void|volatile|while)\\b"))),
+                token("function", pattern(compile("\\b[a-z_]\\w*(?=\\s*\\()", CASE_INSENSITIVE))),
+                token("number", pattern(compile("(?:\\b0x(?:[\\da-f]+(?:\\.[\\da-f]*)?|\\.[\\da-f]+)(?:p[+-]?\\d+)?|(?:\\b\\d+(?:\\.\\d*)?|\\B\\.\\d+)(?:e[+-]?\\d+)?)[ful]{0,4}", CASE_INSENSITIVE))),
+                token("operator", pattern(compile(">>=?|<<=?|->|([-+&|:])\\1|[?:~]|[-+*/%&|^!=<>]=?")))
         );
 
         GrammarUtils.insertBeforeToken(c, "string",
+                token("char", pattern(compile("'(?:\\\\(?:\\r\\n|[\\s\\S])|[^'\\\\\\r\\n]){0,32}'"))),
                 token("macro", pattern(
-                        compile("(^\\s*)#\\s*[a-z]+(?:[^\\r\\n\\\\]|\\\\(?:\\r\\n|[\\s\\S]))*", CASE_INSENSITIVE | MULTILINE),
+                        compile("(^[\\t ]*)#\\s*[a-z](?:[^\\r\\n\\\\/]|/(?!\\*)|/\\*(?:[^*]|\\*(?!/))*\\*/|\\\\(?:\\r\\n|[\\s\\S]))*", CASE_INSENSITIVE | MULTILINE),
                         true,
-                        false,
+                        true,
                         "property",
                         grammar("inside",
-                                token("string", pattern(compile("(#\\s*include\\s*)(?:<.+?>|(\"|')(?:\\\\?.)+?\\2)"), true)),
+                                token("string",
+                                        pattern(
+                                                compile("^(#\\s*include\\s*)<[^>]+>"),
+                                                true),
+                                        Objects.requireNonNull(GrammarUtils.findToken(GrammarUtils.require(prism4j, "clike"), "string")).patterns().get(0)),
+                                commentToken,
+                                token("macro-name",
+                                        pattern(compile("(^#\\s*define\\s+)\\w+\\b(?!\\()", CASE_INSENSITIVE), true),
+                                        pattern(compile("(^#\\s*define\\s+)\\w+\\b(?=\\()", CASE_INSENSITIVE), true, false, "function")),
                                 token("directive", pattern(
-                                        compile("(#\\s*)\\b(?:define|defined|elif|else|endif|error|ifdef|ifndef|if|import|include|line|pragma|undef|using)\\b"),
+                                        compile("^(#\\s*)[a-z]+"),
                                         true,
                                         false,
                                         "keyword"
-                                ))
+                                )),
+                                token("directive-hash", pattern(compile("^#"))),
+                                token("punctuation", pattern(compile("##|\\\\(?=[\\r\\n])"))),
+                                token("expression", pattern(compile("\\S[\\s\\S]*"), false, false, null, c))
                         )
                 )),
                 token("constant", pattern(compile("\\b(?:__FILE__|__LINE__|__DATE__|__TIME__|__TIMESTAMP__|__func__|EOF|NULL|SEEK_CUR|SEEK_END|SEEK_SET|stdin|stdout|stderr)\\b")))
--- prism4j/src/main/java/io/noties/prism4j/languages/Prism_cpp.java
+++ prism4j/src/main/java/io/noties/prism4j/languages/Prism_cpp.java
@@ -5,8 +5,7 @@ import io.noties.prism4j.Prism4j;
 import io.noties.prism4j.annotations.Extend;
 import org.jetbrains.annotations.NotNull;
 
-import static io.noties.prism4j.Prism4j.pattern;
-import static io.noties.prism4j.Prism4j.token;
+import static io.noties.prism4j.Prism4j.*;
 import static java.util.regex.Pattern.CASE_INSENSITIVE;
 import static java.util.regex.Pattern.compile;
 
@@ -17,24 +16,53 @@ public class Prism_cpp {
     @NotNull
     public static Prism4j.Grammar create(@NotNull Prism4j prism4j) {
 
+        String keywordPattern = "\\b(?:alignas|alignof|asm|auto|bool|break|case|catch|char|char8_t|char16_t|char32_t|class|compl|concept|const|consteval|constexpr|constinit|const_cast|continue|co_await|co_return|co_yield|decltype|default|delete|do|double|dynamic_cast|else|enum|explicit|export|extern|final|float|for|friend|goto|if|import|inline|int|int8_t|int16_t|int32_t|int64_t|uint8_t|uint16_t|uint32_t|uint64_t|long|module|mutable|namespace|new|noexcept|nullptr|operator|override|private|protected|public|register|reinterpret_cast|requires|return|short|signed|sizeof|static|static_assert|static_cast|struct|switch|template|this|thread_local|throw|try|typedef|typeid|typename|union|unsigned|using|virtual|void|volatile|wchar_t|while)\\b";
+
         final Prism4j.Grammar cpp = GrammarUtils.extend(
                 GrammarUtils.require(prism4j, "c"),
                 "cpp",
-                token("keyword", pattern(compile("\\b(?:alignas|alignof|asm|auto|bool|break|case|catch|char|char16_t|char32_t|class|compl|const|constexpr|const_cast|continue|decltype|default|delete|do|double|dynamic_cast|else|enum|explicit|export|extern|float|for|friend|goto|if|inline|int|int8_t|int16_t|int32_t|int64_t|uint8_t|uint16_t|uint32_t|uint64_t|long|mutable|namespace|new|noexcept|nullptr|operator|private|protected|public|register|reinterpret_cast|return|short|signed|sizeof|static|static_assert|static_cast|struct|switch|template|this|thread_local|throw|try|typedef|typeid|typename|union|unsigned|using|virtual|void|volatile|wchar_t|while)\\b"))),
-                token("operator", pattern(compile("--?|\\+\\+?|!=?|<{1,2}=?|>{1,2}=?|->|:{1,2}|={1,2}|\\^|~|%|&{1,2}|\\|\\|?|\\?|\\*|\\/|\\b(?:and|and_eq|bitand|bitor|not|not_eq|or|or_eq|xor|xor_eq)\\b")))
+                token("class-name",
+                        pattern(compile("(\\b(?:class|concept|enum|struct|typename)\\s+)(?!" + keywordPattern + ")\\w+"), true),
+                        pattern(compile("\\b[A-Z]\\w*(?=\\s*::\\s*\\w+\\s*\\()")),
+                        pattern(compile("\\b[A-Z_]\\w*(?=\\s*::\\s*~\\w+\\s*\\()", CASE_INSENSITIVE)),
+                        pattern(compile("\\b\\w+(?=\\s*<(?:[^<>]|<(?:[^<>]|<[^<>]*>)*>)*>\\s*::\\s*\\w+\\s*\\()"))
+                ),
+                token("keyword", pattern(compile(keywordPattern))),
+                token("number", pattern(compile("(?:\\b0b[01']+|\\b0x(?:[\\da-f']+(?:\\.[\\da-f']*)?|\\.[\\da-f']+)(?:p[+-]?[\\d']+)?|(?:\\b[\\d']+(?:\\.[\\d']*)?|\\B\\.[\\d']+)(?:e[+-]?[\\d']+)?)[ful]{0,4}", CASE_INSENSITIVE), false, true)),
+                token("operator", pattern(compile(">>=?|<<=?|->|--|\\+\\+|&&|\\|\\||[?:~]|<=>|[-+*/%&|^!=<>]=?|\\b(?:and|and_eq|bitand|bitor|not|not_eq|or|or_eq|xor|xor_eq)\\b")))
         );
 
-        // in prism-js cpp is extending c, but c has not booleans... (like classes)
         GrammarUtils.insertBeforeToken(cpp, "function",
                 token("boolean", pattern(compile("\\b(?:true|false)\\b")))
         );
 
-        GrammarUtils.insertBeforeToken(cpp, "keyword",
-                token("class-name", pattern(compile("(class\\s+)\\w+", CASE_INSENSITIVE), true))
+        GrammarUtils.insertBeforeToken(cpp, "string",
+                token("module", pattern(
+                        compile("(\\b(?:module|import)\\s+)(?:\"(?:\\\\(?:\\r\\n|[\\s\\S])|[^\"\\\\\\r\\n])*\"|<[^<>\\r\\n]*>|\\b(?!" + keywordPattern + ")\\w+(?:\\s*\\.\\s*\\w+)*\\b(?:\\s*:\\s*\\b(?!" + keywordPattern + ")\\w+(?:\\s*\\.\\s*\\w+)*\\b)?|:\\s*\\b(?!" + keywordPattern + ")\\w+(?:\\s*\\.\\s*\\w+)*\\b)"),
+                        true, true, null,
+                        grammar("inside", token("string", pattern(compile("^[<\"][\\s\\S]+"))), token("operator", pattern(compile(":"))), token("punctuation", pattern(compile("\\."))))
+                )),
+                token("raw-string", pattern(compile("R\"([^()\\\\ ]{0,16})\\([\\s\\S]*?\\)\\1\""), false, true, "string"))
         );
 
-        GrammarUtils.insertBeforeToken(cpp, "string",
-                token("raw-string", pattern(compile("R\"([^()\\\\ ]{0,16})\\([\\s\\S]*?\\)\\1\""), false, true, "string"))
+        GrammarUtils.insertBeforeToken(cpp, "keyword",
+                token("generic-function", pattern(compile("\\b(?!operator\\b)[a-z_]\\w*\\s*<(?:[^<>]|<[^<>])*>*>(?=\\s*\\()", CASE_INSENSITIVE),
+                        false, false, null,
+                        grammar("inside",
+                                token("function", pattern(compile("^\\w+"))),
+                                token("generic", pattern(compile("<[\\s\\S]+"),
+                                        false, false, "class-name", cpp)))
+                        )
+                )
+        );
+
+        GrammarUtils.insertBeforeToken(cpp, "operator", token("double-colon", pattern(compile("::"))));
+
+        final Prism4j.Grammar baseClause = GrammarUtils.clone(cpp);
+        GrammarUtils.insertBeforeToken(baseClause, "operator", token("class-name", pattern(compile("\\b[a-z_]\\w*\\b(?!\\s*::)", CASE_INSENSITIVE), false, true)));
+
+        GrammarUtils.insertBeforeToken(cpp, "class-name",
+                token("base-clause", pattern(compile("(\\b(?:class|struct)\\s+\\w+\\s*:\\s*)[^;{}\"']+?(?=\\s*[;{])"), true, true, null, baseClause))
         );
 
         return cpp;
