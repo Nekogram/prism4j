5782506a00374131fced81394fd2573d8c322b81
qwerty287 <ndev@web.de>
2022-01-11 19:57:31 +0100

    Sync Scala

--- prism4j/src/main/java/io/noties/prism4j/languages/Prism_scala.java
+++ prism4j/src/main/java/io/noties/prism4j/languages/Prism_scala.java
@@ -5,8 +5,7 @@ import io.noties.prism4j.Prism4j;
 import io.noties.prism4j.annotations.Extend;
 import org.jetbrains.annotations.NotNull;
 
-import static io.noties.prism4j.Prism4j.pattern;
-import static io.noties.prism4j.Prism4j.token;
+import static io.noties.prism4j.Prism4j.*;
 import static java.util.regex.Pattern.CASE_INSENSITIVE;
 import static java.util.regex.Pattern.compile;
 
@@ -19,20 +18,17 @@ public class Prism_scala {
         final Prism4j.Grammar scala = GrammarUtils.extend(
                 GrammarUtils.require(prism4j, "java"),
                 "scala",
-                new GrammarUtils.TokenFilter() {
-                    @Override
-                    public boolean test(@NotNull Prism4j.Token token) {
-                        final String name = token.name();
-                        return !"class-name".equals(name) && !"function".equals(name);
-                    }
+                token -> {
+                    final String name = token.name();
+                    return !"class-name".equals(name) && !"function".equals(name);
                 },
+                token("triple-quoted-string", pattern(compile("\"\"\"[\\s\\S]*?\"\"\""), false, true, "string")),
+                token("string",
+                        pattern(compile("(\"|')(?:\\\\.|(?!\\1)[^\\\\\\r\\n])*\\1"), false, true)
+                ),
                 token("keyword", pattern(
                         compile("<-|=>|\\b(?:abstract|case|catch|class|def|do|else|extends|final|finally|for|forSome|if|implicit|import|lazy|match|new|null|object|override|package|private|protected|return|sealed|self|super|this|throw|trait|try|type|val|var|while|with|yield)\\b")
                 )),
-                token("string",
-                        pattern(compile("\"\"\"[\\s\\S]*?\"\"\""), false, true),
-                        pattern(compile("(\"|')(?:\\\\.|(?!\\1)[^\\\\\\r\\n])*\\1"), false, true)
-                ),
                 token("number", pattern(
                         compile("\\b0x[\\da-f]*\\.?[\\da-f]+|(?:\\b\\d+\\.?\\d*|\\B\\.\\d+)(?:e\\d+)?[dfl]?", CASE_INSENSITIVE)
                 ))
@@ -44,6 +40,26 @@ public class Prism_scala {
 
         GrammarUtils.insertBeforeToken(scala, "number",
                 token("builtin", pattern(compile("\\b(?:String|Int|Long|Short|Byte|Boolean|Double|Float|Char|Any|AnyRef|AnyVal|Unit|Nothing)\\b")))
+        );
+
+        GrammarUtils.insertBeforeToken(scala, "triple-quoted-string",
+                token("string-interpolation",
+                        pattern(compile("\\b[a-z]\\w*(?:\"\"\"(?:[^$]|\\$(?:[^{]|\\{(?:[^{}]|\\{[^{}]*\\})*\\}))*?\"\"\"|\"(?:[^$\"\\r\\n]|\\$(?:[^{]|\\{(?:[^{}]|\\{[^{}]*\\})*\\}))*\")", CASE_INSENSITIVE),
+                                false, true, null, grammar("inside",
+                                        token("id", pattern(compile("^\\w+"), false, true, "function")),
+                                        token("escape", pattern(compile("\\\\\\$\"|\\$[$\"]"), false, true, "symbol")),
+                                        token("interpolation",
+                                                pattern(compile("\\$(?:\\w+|\\{(?:[^{}]|\\{[^{}]*\\})*\\})"), false, true, null,
+                                                        grammar("inside",
+                                                                token("punctuation", pattern(compile("^\\$\\{?|\\}$"))),
+                                                                token("expression", pattern(compile("[\\s\\S]+"), false, false, null, scala))
+                                                        )
+                                                )
+                                        ),
+                                        token("string", pattern(compile("[\\s\\S]+")))
+                                )
+                        )
+                )
         );
 
         return scala;
